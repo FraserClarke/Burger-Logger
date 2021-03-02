a93967fe2c7183fbc0b812f70dba454d4fa4bff9
Mona Lisa <clarkefraser1@gmail.com>
2021-03-02 14:11:38 +1100

    Add data to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+("Ozzie Burger", true),
+("Pulled Pork Burger", false),
+("Angus Beef Burger", true),
+("Veggie Burger", false),
+("Bacon and Egg Burger", true),
+("Salmon Burger", false),
+("Halapeno Beef Burger", true),
+("Surprise Meat Burger", false),
+("Soylent Green Burger", false);
