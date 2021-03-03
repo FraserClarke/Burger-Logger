8799cffe12da3405e304fd5d3b848f9f4ae2bcbe
Mona Lisa <clarkefraser1@gmail.com>
2021-03-03 11:06:45 +1100

    models,burger.js. imported orm. Created Orm Functions. Export.

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,22 @@
 // Inside burger.js, import orm.js into burger.js
-
+const orm = require('../config/orm.js');
 
 // Also inside burger.js, create the code that will call the ORM functions 
 // using burger specific input for the ORM.
-
-
+const burger = {
+    selectAll(cb) {
+      orm.all('burgers', (res) => cb(res));
+    },
+    // The variables cols and vals are arrays.
+    insertOne(cols, vals, cb) {
+      orm.insert('burgers', cols, vals, (res) => cb(res));
+    },
+    updateOne(objColVals, condition, cb) {
+      orm.update('burgers', objColVals, condition, (res) => cb(res));
+    },
+    
+  };
+  
 // Export at the end of the burger.js file.
+  module.exports = burger;
+
