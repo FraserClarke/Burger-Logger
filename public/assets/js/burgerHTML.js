d3ff35bfd760a2813ce8ea83426b418107e4560d
Mona Lisa <clarkefraser1@gmail.com>
2021-03-03 12:46:24 +1100

    adding handlebar info to generate html. Added image to assets. burgerHTML file EL

---
+++ public/assets/js/burgerHTML.js
@@ -0,0 +1,15 @@
+// get. used for getting data/reading data. Uses url parameters.
+// post insert new data. uses body for data.
+// put. update data. uses body for data
+
+//  Wait to attach our handlers until the DOM is fully loaded.
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (event) {
+      console.info('DOM loaded');
+    }
+
+
+
+
+
+});
