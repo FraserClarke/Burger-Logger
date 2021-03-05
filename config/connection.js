a0cb5c8b8ee9ef6c6ca11485e041ed7863d0bb4e
Mona Lisa <clarkefraser1@gmail.com>
2021-03-05 15:39:14 +1100

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,28 @@
 const util = require("util");
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else
+  connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port, if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'yourRootPassword'
-  
+
     // Be sure to update with your own MySQL password!
     password: "password", // Incorrect password = Access denied error
     database: "burgers_db", // Make sure this is an existing database
   });
-  
-  connection.connect();
-  
-  connection.query = util.promisify(connection.query);
 
-  module.exports = connection;
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
